246def04c1f025dc3c89437cd3b3eba3b7cb7ab1
Vyacheslav <slavach@imperialeads.com>
2024-03-03 16:09:48 +0300

    add form for the hotel room booking

--- resources/js/Components/Dialog/CustomDialog.tsx
+++ resources/js/Components/Dialog/CustomDialog.tsx
@@ -15,40 +15,97 @@ export function CustomDialog() {
     return (
         <Dialog>
             <DialogTrigger asChild>
-                <Button variant="outline">Edit Profile</Button>
+                <Button
+                    className="my-5 border-black text-black bg-white hover:bg-black hover:text-white"
+                    variant="outline"
+                >
+                    Забронировать
+                </Button>
             </DialogTrigger>
             <DialogContent className="sm:max-w-[425px]">
                 <DialogHeader>
-                    <DialogTitle>Edit profile</DialogTitle>
+                    <DialogTitle>Бронь номера</DialogTitle>
                     <DialogDescription>
-                        Make changes to your profile here. Click save when
-                        you're done.
+                        Заполните свои контактные данные и нажмите на кнопку
+                        "Отправить".
                     </DialogDescription>
                 </DialogHeader>
                 <div className="grid gap-4 py-4">
                     <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="name" className="text-right">
-                            Name
+                        <Label htmlFor="first_name" className="text-right">
+                            Ваше имя
                         </Label>
                         <Input
+                            type="text"
                             id="name"
-                            value="Pedro Duarte"
+                            placeholder="Иван"
                             className="col-span-3"
+                            minLength={2}
+                            maxLength={18}
                         />
                     </div>
                     <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-right">
-                            Username
+                        <Label htmlFor="last_name" className="text-right">
+                            Фамилия
                         </Label>
                         <Input
-                            id="username"
-                            value="@peduarte"
+                            type="text"
+                            id="name"
+                            placeholder="Иванов"
                             className="col-span-3"
+                            minLength={2}
+                            maxLength={24}
+                            required
+                        />
+                    </div>
+
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="middle_name" className="text-right">
+                            Отчество
+                        </Label>
+                        <Input
+                            type="text"
+                            id="name"
+                            placeholder="Иванович"
+                            className="col-span-3"
+                            minLength={5}
+                            maxLength={24}
+                            required
+                        />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="email" className="text-right">
+                            Эл. почта
+                        </Label>
+                        <Input
+                            type="email"
+                            id="email"
+                            placeholder="ivanov@gmail.com"
+                            className="col-span-3"
+                            required
+                        />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="phoneNumber" className="text-right">
+                            Контактный номер
+                        </Label>
+                        <Input
+                            type="tel"
+                            id="tel"
+                            placeholder="+79787812345"
+                            className="col-span-3"
+                            required
                         />
                     </div>
                 </div>
                 <DialogFooter>
-                    <Button type="submit">Save changes</Button>
+                    <Button
+                        className="border-black text-black bg-white hover:bg-black hover:text-white"
+                        variant="outline"
+                        type="submit"
+                    >
+                        Отправить
+                    </Button>
                 </DialogFooter>
             </DialogContent>
         </Dialog>

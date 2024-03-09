e44981cc76eae4c59b86da2e8f903ccc9a4ad896
Vyacheslav <slavach@imperialeads.com>
2024-03-09 16:51:43 +0300

    update customer controller and alert component

--- resources/js/Components/Alert.tsx
+++ resources/js/Components/Alert.tsx
@@ -1,32 +1,41 @@
-import { Alert, AlertDescription, AlertTitle } from "@/Components/readyToUse/alert";
+import {
+    Alert,
+    AlertDescription,
+    AlertTitle,
+} from "@/Components/readyToUse/alert";
 import { Terminal } from "lucide-react";
-import { motion} from "framer-motion";
-
+import { motion } from "framer-motion";
 
 const exitAnimation = {
-  hidden: { opacity: 0, y: 20 },
-  visible: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: 20 },
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: 20 },
 };
 
 interface AlertCompProps {
-  show: boolean;
+    show: boolean;
+    title: string;
+    decsription: string;
 }
 
-export function AlertComp({ show }: AlertCompProps) {
-  return (
-    <motion.div
-      initial="hidden"
-      animate={show ? "visible" : "exit"}
-      exit="exit"
-      variants={exitAnimation}
-      className="fixed bottom-4 left-4 z-10"
-    >
-      <Alert>
-        <Terminal className="h-4 w-4" />
-        <AlertTitle>Отлично!</AlertTitle>
-        <AlertDescription>Выберите дату заезда</AlertDescription>
-      </Alert>
-    </motion.div>
-  );
+export function AlertComp({
+    show,
+    title = "Отлично!",
+    decsription,
+}: AlertCompProps) {
+    return (
+        <motion.div
+            initial="hidden"
+            animate={show ? "visible" : "exit"}
+            exit="exit"
+            variants={exitAnimation}
+            className="fixed bottom-4 left-4 z-10"
+        >
+            <Alert>
+                <Terminal className="h-4 w-4" />
+                <AlertTitle>{title}</AlertTitle>
+                <AlertDescription>{decsription}</AlertDescription>
+            </Alert>
+        </motion.div>
+    );
 }

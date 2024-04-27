0f9b76b65ea2929ca8b7c00f7fa5a74d707832bc
Vyacheslav <slavach@imperialeads.com>
2024-04-27 16:59:25 +0300

    add feedbacks total to response

--- app/Infrastructure/Repository/Feedbacks/FeedbacksRepository.php
+++ app/Infrastructure/Repository/Feedbacks/FeedbacksRepository.php
@@ -15,7 +15,7 @@ class FeedbacksRepository implements FeedbacksRepositoryInterface
     {
         $query = Feedback::query();
 
-        if(isset($filters['id'])){
+        if (isset($filters['id'])) {
             $query->where(['id' => $filters['id']]);
         }
 
@@ -68,8 +68,17 @@ class FeedbacksRepository implements FeedbacksRepositoryInterface
         $feedbacks = $query->get()->toArray();
 
         return [
+            'total'     =>  $this->getFeedbacksCount(),
             'feedbacks' => $feedbacks,
         ];
+    }
+
+    /**
+     * @return int
+     */
+    #[\Override] public function getFeedbacksCount(): int
+    {
+        return Feedback::count();
     }
 
     /**
